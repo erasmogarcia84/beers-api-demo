6419313b4dbc99bf9781f24bf273c165938e32a4
Erasmo García González <erasmogarciaglez@gmail.com>
2023-06-17 20:32:50 +0200

    Add main route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ChakraProvider, Box, Grid, theme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
 import BeersList from "./pages/BeersList";
+import { Route, Routes } from "react-router";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
+
+        <Routes>
+          <Route path="/" element={<BeersList />} />
+        </Routes>
         <BeersList />
       </Grid>
     </Box>
