9cd28229eb357c0ad9a86a61c8f8aa306b4b817b
Erasmo García González <erasmogarciaglez@gmail.com>
2023-06-17 21:24:08 +0200

    Add getById method

--- src/services/beers.ts
+++ src/services/beers.ts
@@ -1,10 +1,26 @@
+const API_URL = "https://api.punkapi.com/v2/beers";
+
 export const getBeers = async () => {
   try {
-    const response = await fetch("https://api.punkapi.com/v2/beers");
+    const response = await fetch(API_URL);
     const beers = await response.json();
 
     return beers;
   } catch (error) {
     throw new Error("Error fetching beers: " + error);
+  }
+};
+
+export const getBeerById = async (id: string) => {
+  try {
+    const response = await fetch(`${API_URL}/${id}`);
+    const beer = await response.json();
+
+    if (beer.error) {
+      throw Error;
+    }
+    return beer[0];
+  } catch (error) {
+    throw new Error();
   }
 };
